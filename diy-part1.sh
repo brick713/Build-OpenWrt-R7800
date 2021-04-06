0c2a7975aed219803679202985245e3679e8903d
Brick713 <brick@Brick713deMacBook-Pro.local>
2021-04-06 09:44:51 +0800

    Change build config

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,5 +13,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
-sed -i "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages'  feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small'  feeds.conf.default
