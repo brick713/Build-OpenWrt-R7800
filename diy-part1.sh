1de2b5a3024040f4760a3dfe209d142786482025
Brick713 <brick@Brick713deMacBook-Pro.local>
2021-04-06 09:36:34 +0800

    Change build config

---
+++ diy-part1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+sed -i "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
