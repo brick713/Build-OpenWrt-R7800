1aa371256ff560e432cc6620ad59ddd76e1e5e54
Brick713 <brick@Brick713deMacBook-Pro.local>
2021-07-21 11:49:10 +0800

    add ddns

--- diy-part.sh
+++ diy-part.sh
@@ -12,3 +12,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
+
