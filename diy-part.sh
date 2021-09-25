395fc69edc90d43796b4b5282ab742539aedb01e
Brick713 <brick@Brick713deMBP.lan>
2021-09-25 22:57:46 +0800

    change config

--- diy-part.sh
+++ diy-part.sh
@@ -13,5 +13,5 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
-#echo 'src-git nas https://github.com/linkease/nas-packages' >> feeds.conf.default
-#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci' >> feeds.conf.default
